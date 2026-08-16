66e50938c57512d28c3408a74f4f48d8ad4c8188
Sri-Ai-Innovative <ksriram.btechaids@gmail.com>
2026-08-16 07:44:24 +0530

    fix: use official netlify tanstack start plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tanstackStart({
-    server: {
-      preset: 'netlify'
-    }
-  }), react()],
+  plugins: [
+    netlify(),
+    tailwindcss(), 
+    tanstackStart(), 
+    react()
+  ],
   resolve: {
     tsconfigPaths: true
   }
